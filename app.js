caac41f6cbefdc72895ccce794962025976a3f9d
whitershade <whitershade34@gmail.com>
2018-01-04 20:14:10 +0200

    added skeleton of notes functionality

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const fs = require('fs');
 const os = require('os');
+const { argv } = require('yargs');
 const _ = require('lodash');
 const notes = require('./notes');
 
-fs.appendFile('./greetings.txt', `hello, ${os.userInfo().username}!`, error => {
-  if (error) return console.log(error);
-  console.log('done');
-});
+const command = process.argv[2]; // node app.js THIRD_ARGUMENT
+
+switch (command) {
+  case 'add':
+    notes.add(argv.title, argv.body);
+    break;
+  case 'list':
+    notes.getAll();
+    break;
+  case 'get':
+    notes.get(argv.title);
+    break;
+  case 'remove':
+    notes.remove(argv.title);
+    break;
+  default:
+    console.log('command does not recognize');
+}
--- notes.js
+++ notes.js
@@ -1,2 +1,22 @@
-console.log('notes');
+const add = (title, body) => {
+  console.log(title, body);
+};
 
+const getAll = () => {
+  console.log('get all');
+};
+
+const get = title => {
+  console.log('read: ' + title);
+};
+
+const remove = title => {
+  console.log('remove: ' + title);
+};
+
+module.exports = {
+  add,
+  getAll,
+  get,
+  remove
+};
