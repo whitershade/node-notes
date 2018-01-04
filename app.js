289dec4309098f7a1d451a04288bade65aa74c8f
whitershade <whitershade34@gmail.com>
2018-01-05 01:53:40 +0200

    added advanced yargs command with required arguments and showing help

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 const fs = require('fs');
 const os = require('os');
-const { argv } = require('yargs');
+const yargs = require('yargs');
 const _ = require('lodash');
 const notes = require('./notes');
 
 const command = process.argv[2]; // node app.js THIRD_ARGUMENT
+
+const title = {
+  describe: 'Title of note',
+  demand: true,
+  alias: 't'
+};
+
+const body = {
+  describe: 'Body of note',
+  demand: true,
+  alias: 'b'
+};
+
+const argv = yargs
+  .command('add', 'Add a new note', {
+    title,
+    body
+  })
+  .command('list', 'List all notes')
+  .command('get', 'Get a note by title', {
+    title
+  })
+  .command('remove', 'Remove a note by title', {
+    title
+  })
+  .help().argv;
 
 switch (command) {
   case 'add':
